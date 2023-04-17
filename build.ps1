42d4d0274424c798e5626ba3f4d6fa74840c71ce
Sanjay Govind <sanjay.govind9@gmail.com>
2023-04-17 20:46:26 +1200

    add ps1

--- build.ps1
+++ build.ps1
@@ -1,31 +1,26 @@
-#!/usr/bin/env bash
-PLATFORM_IO_VERSION=6.1.5
-PYTHON_URL_BASE="https://github.com/indygreg/python-build-standalone/releases/download/"
-PYTHON_RELEASE="20230116"
-PYTHON_VERSION="3.11.1"
-PYTHON_ARCH="x86_64"
-PYTHON="`pwd`/build/python/bin/python3"
-case "$OSTYPE" in
-  linux*)   PYTHON_ARCH="$PYTHON_ARCH-unknown-linux-gnu" ;;
-  darwin*)  PYTHON_ARCH="$PYTHON_ARCH-apple-darwin" ;; 
-  *)        echo "unknown: $OSTYPE" ;;
-esac
+$PLATFORM_IO_VERSION=6.1.5
+$PYTHON_URL_BASE="https://github.com/indygreg/python-build-standalone/releases/download/"
+$PYTHON_RELEASE="20230116"
+$PYTHON_VERSION="3.11.1"
+$PYTHON_ARCH="x86_64"
+$PYTHON="`pwd`/build/python/bin/python3"
+$PYTHON_ARCH="$PYTHON_ARCH-pc-windows-msvc-shared"
 
-PYTHON_URL="$PYTHON_URL_BASE/$PYTHON_RELEASE/cpython-$PYTHON_VERSION+$PYTHON_RELEASE-$PYTHON_ARCH-install_only.tar.gz"
-echo downloading $PYTHON_URL
+$PYTHON_URL="${PYTHON_URL_BASE}/${PYTHON_RELEASE}/cpython-${PYTHON_VERSION}+$PYTHON_RELEASE-$PYTHON_ARCH-install_only.tar.gz"
+echo "downloading ${PYTHON_URL}"
 mkdir build
 cd build
 wget $PYTHON_URL -O python.tar.gz
 tar -xzf python.tar.gz
 rm -rf python.tar.gz
-$PYTHON -m pip install platformio==$PLATFORM_IO_VERSION
+./$PYTHON -m pip install platformio==$PLATFORM_IO_VERSION
 export PLATFORMIO_CORE_DIR=`pwd`/platformio
 export PYTHONUNBUFFERED=1
 cd ../Ardwiino
-$PYTHON -m platformio pkg install
+./$PYTHON -m platformio pkg install
 rm -rf .pio
 rm -rf ../build/platformio/.cache
-$PYTHON -m platformio pkg install
+./$PYTHON -m platformio pkg install
 cd ../build
 rm -rf platformio/packages/framework-arduinopico/pico-sdk/lib/btstack/port
 rm -rf platformio/packages/framework-arduinopico/pico-sdk/lib/btstack/example
@@ -36,5 +31,4 @@ rm -rf platformio/packages/framework-arduino-avr/bootloaders
 rm -rf platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3
 rm -rf platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s2
 rm -rf platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3
-export XZ_OPT="-T0 -9"
-tar cfJ platformio.tar.xz platformio
+7z.exe a -ttar -so platformio.tar.xz platformio | 7z.exe a -txz -si platformio.tar.xz -mx9
